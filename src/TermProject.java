bd2edfe1a8f3a8816f56a5c504bcec7f18d3cda5
RafesiA <whrudwls98@naver.com>
2017-11-29 01:48:05 +0900

    Panel 좀 만들어주세요 엉엉
    패널따위를 몰라서 막힙니다 엉엉 시간나면 해주세요

--- src/TermProject.java
+++ src/TermProject.java
@@ -2,9 +2,26 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.filechooser.*;
+import java.util.*;
+import java.io.*;
 
 public class TermProject extends JFrame {
+	JTextArea ew = new JTextArea();
+	
+	class EPanel extends JPanel{
+		EPanel(){
+			setVisible(true);
+			setSize(10, 10);
+			setLayout(new BorderLayout());
+			ew.setSize(10, 10);
+			add(ew,BorderLayout.CENTER);
+			add(new JScrollPane(ew));
+		}
+	}
+	
 	public TermProject() {
+		EPanel e = new EPanel();
+		e.setLocation(0, 10);
 		setTitle("Java IDE");
 		createMenu();
 		setSize(600,600);
@@ -13,8 +30,7 @@ public class TermProject extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c;
 		c = getContentPane();
-		JTextArea ew = new JTextArea();
-		c.add(ew, BorderLayout.CENTER);
+		c.add(e);
 	}
 	private void createMenu() {
 		JMenuBar mb = new JMenuBar();
@@ -52,12 +68,33 @@ public class TermProject extends JFrame {
 			String fileName = null;
 			switch(command) {
 				case "Open":
-					int ret = chooser.showOpenDialog(null);
-					if(ret != JFileChooser.APPROVE_OPTION) {
-						JOptionPane.showMessageDialog(null, "파일은 선택하지 않았습니다.", "Warning", JOptionPane.WARNING_MESSAGE);
+					try {
+						int ret = chooser.showOpenDialog(null);
+						if(ret != JFileChooser.APPROVE_OPTION) {
+							JOptionPane.showMessageDialog(null, "파일은 선택하지 않았습니다.", "Warning", JOptionPane.WARNING_MESSAGE);
+						}
+						fileName = chooser.getSelectedFile().getAbsolutePath();
+						File javaFile = new File(fileName);
+						BufferedReader br = new BufferedReader(new FileReader(javaFile));
+						ew.read(br, javaFile);
+						br.close();
+					} catch(IOException err) {
+						String er = err.getMessage();
+						ew.append(er);
 					}
-					fileName = chooser.getSelectedFile().getAbsolutePath();
-					System.out.println(fileName);
+						break;
+				case "Close":
+					//Close Function
+					
+				case "Save":
+					//Save Function
+					
+				case "Save As":
+					//Save As Function
+					
+				case "Quit":
+					System.exit(0);
+					//Quit Function
 			
 			}
 		}
